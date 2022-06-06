6e35511b8b9c1a52b2d6411e63604341f808594e
Kurt Baux <ktripodibaux@gmail.com>
2022-06-06 15:46:23 -0400

    commit tree cfed2c9f60b034cb7b2bfb417aefcda36e9ed48f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(42 - location);
+}
+function distanceFromHqInFeet (location) {
+    return distanceFromHqInBlocks(location) *264;
+}
+function distanceTravelledInFeet (location1, location2) {
+    return Math.abs(location1 - location2) * 264;
+}
+function calculatesFarePrice(location1, location2) {
+    let distance = distanceTravelledInFeet (location1, location2);
+    if (distance <= 400) {
+        return 0;
+    }
+    if (distance <= 2000) {
+        distance -= 400;
+        return distance * .02;
+    }
+    if (distance>= 2500) {
+        return 'cannot travel that far'
+    }
+    else {
+        return 25;
+    }
+}
